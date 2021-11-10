71cacc7b70c6f4b3de33ae7f4003fe1b91b63662
Reydesel C <cuevasr@email.arizona.edu>
2021-11-09 22:15:54 -0700

    installed nodemailer and created models for device and token

---
+++ Project/public/javascripts/register.js
@@ -0,0 +1,62 @@
+function Register() {
+  let email = $('#email').val();
+  let password = $('#password').val();
+  let fullName = $('#fullName').val();
+  let passwordConfirm = $('#passwordConfirm').val();
+
+
+  var strongRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+
+  if(!strongRegex.test(password)){
+    $('#ServerResponse').html("<span class='red-text text-darken-2'>Password is not strong enough.:"
+                              +"<ul> <li>length of 8 or more characters</li>"
+                              +"<li>At least one Capital letter</li>"
+                              +"<li>At least one lower case letter</li>"
+                              +"<li>At least one number</li>"
+                              +"<li>At least one lower Special Character</li>"
+                              +"</ul></span>");
+    $('#ServerResponse').show();
+    return;
+  }
+  
+  if (password != passwordConfirm) {
+    $('#ServerResponse').html("<span class='red-text text-darken-2'>Passwords don't match.</span>");
+    $('#ServerResponse').show();
+    return;
+  }
+
+  $.ajax({
+   url: '/users/register',
+   type: 'POST',
+   contentType: 'application/json',
+   data: JSON.stringify({email:email, fullName:fullName, password:password}),
+   dataType: 'json'
+  })
+    .done(registerSuccess)
+    .fail(registerError);
+}
+
+function registerSuccess(data, textStatus, jqXHR) {
+  if (data.success) {
+    window.location = "signin.html";
+  }
+  else {
+    $('#ServerResponse').html("<span class='red-text text-darken-2'>Error: " + data.message + "</span>");
+    $('#ServerResponse').show();
+  }
+}
+
+function registerError(jqXHR, textStatus, errorThrown) {
+  if (jqXHR.statusCode == 404) {
+    $('#ServerResponse').html("<span class='red-text text-darken-2'>Server could not be reached.</p>");
+    $('#ServerResponse').show();
+  }
+  else {
+    $('#ServerResponse').html("<span class='red-text text-darken-2'>Error: " + jqXHR.responseJSON.message + "</span>");
+    $('#ServerResponse').show();
+  }
+}
+
+$(function () {
+  $('#signup').click(Register);
+});

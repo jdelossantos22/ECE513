340203441f61515850181ff553d816dd4e8bb8ec
jdelossantos22@email.arizona.edu <jdelossantos22@email.arizona.edu>
2021-11-26 20:09:27 -0700

    fixing duplicate code

--- AWS/app.js
+++ AWS/app.js
@@ -14,10 +14,10 @@ var app = express();
 /* view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');*/
-
+app.set('views', path.join(__dirname, 'views'));
 app.set('public', path.join(__dirname, 'public'));
 // Set EJS View Engine**
-app.set('view engine','ejs');
+app.set('view engine', 'jade');
 // Set HTML engine**
 app.engine('html', require('ejs').renderFile);
 

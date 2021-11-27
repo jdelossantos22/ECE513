040a57a4f97961ef30b11b6042697800a371af19
jdelossantos22@email.arizona.edu <jdelossantos22@email.arizona.edu>
2021-11-26 20:32:08 -0700

    adding stuff

---
+++ Particle/Firmware/target/2.2.0-rc.2/argon/obj/src/user_module.o.d
@@ -0,0 +1,8 @@
+h:/Documents/Masters/ECE513/Project/ECE513/Particle/Firmware/target/2.2.0-rc.2/argon//obj/./src/user_module.o: \
+ src/user_module.c \
+ ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
+ ../../../dynalib/inc/dynalib.h
+
+../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
+
+../../../dynalib/inc/dynalib.h:

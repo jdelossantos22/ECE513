d9c2ebbafbaeb09d9749117fc7d074bec06a09ee
Reydesel C <cuevasr@email.arizona.edu>
2021-12-04 14:51:17 -0700

    Added Javscript for account

---
+++ AWS/public/javascripts/account.js
@@ -0,0 +1,50 @@
+function sendReqForAccountInfo() {
+  $.ajax({
+    url: '/users/account',
+    type: 'GET',
+    headers: { 'x-auth': window.localStorage.getItem("authToken") },
+    dataType: 'json'
+  })
+    .done(accountInfoSuccess)
+    .fail(accountInfoError);
+}
+
+function accountInfoSuccess(data, textSatus, jqXHR) {
+  $("#email").html(data.email);
+  $("#fullName").html(data.fullName);
+  $("#lastAccess").html(data.lastAccess);
+  $("#zip").html(data.zip);
+  $("#main").show();
+  
+  // Add the devices to the list before the list item for the add device button
+  for (let device of data.devices) {
+    $("#addDeviceList").before("<li class='collection-item'>ID: " +
+      "<span class='ID'> " + device.deviceId + "</span>, APIKEY: <span class='ID'> " + device.apikey + "</span><br>" +
+      " </li>");
+  }
+}
+
+function accountInfoError(jqXHR, textStatus, errorThrown) {
+  // If authentication error, delete the authToken 
+  // redirect user to sign-in page 
+  if( jqXHR.status === 401 ) {
+    window.localStorage.removeItem("authToken");
+    window.location.replace("signin.html");
+  } 
+  else {
+    $("#error").html("Error: " + status.message);
+    $("#error").show();
+  } 
+}
+
+// Handle authentication on page load
+$(function() {
+  // If there's no authToekn stored, redirect user to 
+  // the sign-in page 
+  if (!window.localStorage.getItem("authToken")) {
+    window.location.replace("signin.html");
+  }
+  else {
+    sendReqForAccountInfo();
+  }
+});

1eb0b4b059f6e10e3ae9be6c6d5d290eea133355
ilbell <56981005+ilbell@users.noreply.github.com>
2021-12-08 23:09:34 -0700

    Update smartlight.cpp

--- Particle/v3/src/smartlight.cpp
+++ Particle/v3/src/smartlight.cpp
@@ -6,10 +6,10 @@ CSmartLight::CSmartLight() {
     brightness = RGB_BRIGHTNESS_DEAULT;
     sensorMax = LIGHT_SENSOR_MAX;
     sensorMin = LIGHT_SENSOR_MIN;
-    //color = "#ffffff";
-    bedtimeStart = -1;
-    wakeupStart = 0;
-
+    color = 16777215;   //ffffff (white)
+    bedtimeStart = 72000000;
+    wakeupStart = 21600000;
+    
     statusStr = "{}";
     resetCmd();
 }
@@ -32,9 +32,9 @@ void CSmartLight::cmdProcessing(JSONValue cmdJson) {
         else if (iter.name() == "max") {
             sensorMax = iter.value().toInt();
         }
-        //else if (iter.name() == "color") {
-        //    color = iter.value().toString();
-        //}
+        else if (iter.name() == "color") {
+            color = iter.value().toInt();
+        }
         else if (iter.name() == "bedtime") {
             bedtimeStart = iter.value().toInt();        //In milliseconds (See controllerAtHome.html)
         }
@@ -54,16 +54,16 @@ void CSmartLight::execute() {
                     state_L1 = CSmartLight::S_MANUAL;
                 }
             }
-            /*else if (!(bedtimeActive())) {
+            else if (!(bedtimeActive())) {
                 state_L0 = CSmartLight::S_ON;
                 state_L1 = CSmartLight::S_MANUAL;
-            }*/
+            }
             break;
         case CSmartLight::S_ON:
             switch (state_L1) {
                 case CSmartLight::S_MANUAL:
                     updateBrightnessManually(cmd.Brightness);
-                    //updateColor();
+                    updateColor();
                     if (cmd.Auto != INVALID_CMD) {
                         if(cmd.Auto) state_L1 = CSmartLight::S_AUTO;
                     }
@@ -71,7 +71,7 @@ void CSmartLight::execute() {
                 
                 case CSmartLight::S_AUTO:
                     updataBrightnessAutomatically();
-                    //updateColor();
+                    updateColor();
                     if (cmd.Auto != INVALID_CMD) {
                         if(!cmd.Auto) state_L1 = CSmartLight::S_MANUAL;
                     }
@@ -83,9 +83,9 @@ void CSmartLight::execute() {
             if (cmd.On != INVALID_CMD) {
                 if(!cmd.On) state_L0= CSmartLight::S_OFF;
             }
-            //else if (bedtimeActive()) {
-            //    state_L0 = CSmartLight::S_OFF;
-            //}
+            else if (bedtimeActive()) {
+                state_L0 = CSmartLight::S_OFF;
+            }
             break;
         default:
             break;
@@ -127,20 +127,82 @@ void CSmartLight::updataBrightnessAutomatically() {
     RGB.brightness(brightness);
 }
 
-//void CSmartLight::updateColor() {
-//    RGB.color(color);
-//}
-/*
-bool CSmartLight::bedtimeActive() {
-    int currentTime = millis();
+void CSmartLight::updateColor() {
+    char colorHex[7];
+    
+    snprintf(colorHex, sizeof colorHex, "%06lx", color);
+    puts(colorHex);
 
-    if ((currentTime % bedtimeStart) > bedtimeStart && (currentTime % wakeupStart) < wakeupStart) {
-        return true;
+    int RGB1 = 0;
+    int RGB2 = 0;
+    int RGB3 = 0;
+
+    int value = 0;
+    int length = 1;
+
+    //Find the decimal representation of RGB1
+    for(int i = 0; i <= 1; i++) {
+        if(colorHex[i]>='0' && colorHex[i]<='9')
+        {
+            value = colorHex[i] - 48;
+        }
+        else if(colorHex[i]>='a' && colorHex[i]<='f')
+        {
+            value = colorHex[i] - 97 + 10;
+        }
+
+        RGB1 += value * pow(16, length);
+        length--;
+    }
+
+    length = 1;
+
+    //Find the decimal representation of RGB2
+    for(int i = 2; i <= 3; i++) {
+        if(colorHex[i]>='0' && colorHex[i]<='9')
+        {
+            value = colorHex[i] - 48;
+        }
+        else if(colorHex[i]>='a' && colorHex[i]<='f')
+        {
+            value = colorHex[i] - 97 + 10;
+        }
+
+        RGB2 += value * pow(16, length);
+        length--;
+    }
+
+    length = 1;
+
+    //Find the decimal representation of RGB3
+    for(int i = 4; i <= 5; i++) {
+        if(colorHex[i]>='0' && colorHex[i]<='9')
+        {
+            value = colorHex[i] - 48;
+        }
+        else if(colorHex[i]>='a' && colorHex[i]<='f')
+        {
+            value = colorHex[i] - 97 + 10;
+        }
+
+        RGB3 += value * pow(16, length);
+        length--;
+    }
+    RGB.color(RGB1, RGB2, RGB3);
+}
+
+bool CSmartLight::bedtimeActive() {
+    int currentTime = (int)(Time.hour() * 60 * 60 * 1000 + (int)Time.minute() * 60 * 1000 + Time.second() * 1000) / 24;
+    bedtimeStart /= 3600;
+    wakeupStart /= 3600;
+
+    if ((currentTime > bedtimeStart) && (currentTime < wakeupStart)) {
+        return false;
     }
     else {
         return false;
     }
-}*/
+}
 
 void CSmartLight::readSensorVal() {
     sensorVal = analogRead(LIGHT_SENSOR);
@@ -152,6 +214,6 @@ int CSmartLight::getSensorVal() {
 
 
 void CSmartLight::createStatusStr() {
-    statusStr = String::format("{\"L0\":%d,\"L1\":%d,\"b\":%d,\"s\":%d,\"m\":%d,\"M\":%d}", 
-        state_L0, state_L1, (int)((double)brightness/RGB_BRIGHTNESS_MAX*100.0), sensorVal, sensorMin, sensorMax);
+    statusStr = String::format("{\"L0\":%d,\"L1\":%d,\"b\":%d,\"s\":%d,\"m\":%d,\"M\":%d,\"D\":%d}", 
+        state_L0, state_L1, (int)((double)brightness/RGB_BRIGHTNESS_MAX*100.0), sensorVal, sensorMin, sensorMax, color);
 }

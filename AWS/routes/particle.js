b3eee2b45c215648126a264d27a6df158e8f7350
jdelossantos22@email.arizona.edu <jdelossantos22@email.arizona.edu>
2021-12-09 22:09:49 -0700

    updating device in particle ROUTE

--- AWS/routes/particle.js
+++ AWS/routes/particle.js
@@ -29,8 +29,8 @@ router.post('/device', function(req,res, next){
     let device = req.body.device;
     deviceInfo.id = device.id;
     deviceInfo.token = device.token;
-    console.log(deviceInfo.startDate);
-    referenceTimeInSec = new Date(deviceInfo.startDate)/1000;
+    console.log(device.startDate);
+    referenceTimeInSec = new Date(device.startDate)/1000;
     console.log(referenceTimeInSec)
     res.status(201).json({success : true, message : "Device updated for the particle", data:JSON.stringify(deviceInfo)});
 });

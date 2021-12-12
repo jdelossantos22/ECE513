5aaa78e4f9d346d3977d36daaa2537d856afbecb
jdelossantos22@email.arizona.edu <jdelossantos22@email.arizona.edu>
2021-12-11 21:53:26 -0700

    working on temperature history

--- AWS/routes/temperature.js
+++ AWS/routes/temperature.js
@@ -54,23 +54,25 @@ router.post('/readAll', function(req,res,next){
     
     let today = new Date(req.body.date);
     
-    today = today.setHours(0,0,0,0);
+    //today = today.setHours(0,0,0,0);
     //console.log()
     console.log(`COMPARE: ${Date.now()}`)
     console.log(`HERE: ${today}`);
     //console.log(day)
-    let tomorrow = today + day;
+    let tomorrow = req.body.date + day;
     tomorrow = new Date(tomorrow)
-    tomorrow = tomorrow.setHours(0,0,0,0);
+    //tomorrow = tomorrow.setHours(0,0,0,0);
     console.log(`HERE: ${tomorrow}`);
     //console.log(today)
     //console.log(tomorrow)
     //{date:{$gt: Date(today), $lt:Date(tomorrow)}}
     //console.log(req.body.id)
-    console.log(new Date(today).toISOString())
-    console.log(new Date(tomorrow).toISOString())
+    today = new Date(today)
+    tomorrow = new Date(tomorrow)
+    console.log(new Date(today.getFullYear(),today.getMonth(),today.getDate(),0,0,0).toISOString())
+    console.log(new Date(today.getFullYear(),today.getMonth(),today.getDate(),0,0,0).toISOString())
     //, $lt: Date(tomorrow)}
-    Temperature.find({postDate:{$gte: new Date(today).toISOString(), $lt: new Date(tomorrow).toISOString()}, deviceId:req.body.id, userEmail:req.body.email}).sort({postDate:1}).exec(function(err, docs){
+    Temperature.find({postDate:{$gte: new Date(today.getFullYear(),today.getMonth(),today.getDate(),0,0,0).toISOString(), $lt: new Date(today.getFullYear(),today.getMonth(),today.getDate(),0,0,0).toISOString()}, deviceId:req.body.id, userEmail:req.body.email}).sort({postDate:1}).exec(function(err, docs){
         if(err){
             let msg = `Can't find information on date ...`;
             res.status(201).json({msg:msg});
